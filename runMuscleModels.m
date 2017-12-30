abd04fe95483a06d8a3ea7e888a0c754ea223fa4
Akira <nagamori@usc.edu>
2017-12-30 12:24:44 -0800

    clean up protocol

--- runMuscleModels.m
+++ runMuscleModels.m
@@ -17,48 +17,27 @@ amp = amp_temp(i);
 input = [zeros(1,1*Fs) amp*[0:1/Fs:1] amp*ones(1,5*Fs),zeros(1,1*Fs)];
     
 output_1 = muscleModel_Song(t,Fs,input,modelParameter,simulationParameter);
-output_2 = muscleModel_Combined(t,Fs,input,modelParameter,simulationParameter,1);
+%output_2 = muscleModel_Combined(t,Fs,input,modelParameter,simulationParameter,1);
 %output_3 = muscleModel_Combined(t,Fs,input,modelParameter,simulationParameter,2);
 
 maxForce1(i) =  mean(output_1.Force_total(6*Fs:7*Fs));
-% halfForce1 = maxForce1(i)/2;
-% [~,loc1] = min(abs(halfForce1-output_1.Force_total(1*Fs:2*Fs)));
-% t_0_50_1(i) = t(loc1+1*Fs)-1;
-
-maxForce2(i) = mean(output_2.Force_total(6*Fs:7*Fs));
-% halfForce2 = maxForce2(i)/2;
-% [~,loc2] = min(abs(halfForce2-output_2.Force_total(1*Fs:2*Fs)));
-% t_0_50_2(i) = t(loc2+1*Fs)-1;
-CoV(i) = std(output_2.Force_total(4*Fs:5*Fs))/mean(output_2.Force_total(4*Fs:5*Fs));
+%maxForce2(i) = mean(output_2.Force_total(6*Fs:7*Fs));
+%CoV(i) = std(output_2.Force_total(4*Fs:5*Fs))/mean(output_2.Force_total(4*Fs:5*Fs));
 
 %%
 figure(1)
 plot(t,output_1.Force_total)
 hold on 
-plot(t,output_2.Force_total)
-hold on 
+% plot(t,output_2.Force_total)
+% hold on 
 %plot(t,output_3.Force_total)
 end
 
 figure(2)
-
 plot(amp_temp,maxForce1./maxForce1(end))
 hold on
-plot(amp_temp,maxForce2./maxForce2(end))
+%plot(amp_temp,maxForce2./maxForce2(end))
 
-% figure(3)
-% plot(amp_temp,t_0_50_1)
-% hold on 
-% plot(amp_temp,t_0_50_2)
+% figure(4)
+% plot(amp_temp,CoV)
 
-figure(4)
-plot(amp_temp,CoV)
-
-% 
-% figure(3)
-% plot(t,output_2.spike_train(450,:))
-% hold on
-% plot(t,output_2.Af(450,:))
-% hold on
-% plot(t,output_2.FF(450,:))
-% 

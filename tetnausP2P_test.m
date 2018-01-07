d0b6d7a787309fb60614ffea617203735ff31f7e
Akira <nagamori@usc.edu>
2018-01-06 23:50:20 -0800

    changes

--- tetnausP2P_test.m
+++ tetnausP2P_test.m
@@ -1,10 +1,10 @@
 %--------------------------------------------------------------------------
-% activationForceRelationship_fit.m
+% tetnausP2P_test.m
 % Author: Akira Nagamori
-% Last update: 12/25/17
+% Last update: 1/5/17
 % Code descriptions
-% Ojbective: fit the relationship between activation (in Hz) and force to
-% the Af relationship (Af function) formulated by Loeb's model
+% Ojbective: Test and compare how peak-to-peak amplitude of tetanus changes
+% with activation level
 %   - Define the minimum and peak firing rate of a given motor unit
 %   - Compute f_0.5 (firing rate at which half of maximum force is achieved)
 %   - Sweep firing rate from minimum (1 Hz) up to 3*f_0.5 and obtain mean
@@ -50,7 +50,7 @@ Lce = 1;
 Y = 1;
 S = 0.96;
 testingUnit =  1;
-FR = 1:1:3*FR_half(testingUnit); %PFR_MU(testingUnit);
+FR = 1:2:3*FR_half(testingUnit); %PFR_MU(testingUnit);
 %FR = 0:10:100; %PFR_MU(testingUnit);
 
 %[twitch,T1,T2] = twitch_function(1,Af,Lce,CT(testingUnit),RT(testingUnit),Fs);
@@ -68,39 +68,46 @@ for i = 1:length(FR)
         FF = frequency2Force_fast_function(f_env,Lce,S);
     end
     Af_vec(i) = Af;
+    
+   
     [twitch,T1(i),T2(i)] = twitch_function(f_env,Af,Lce,CT(testingUnit),RT(testingUnit),Fs);
+    
     twitch = twitch*FF;
     force_temp = conv(spikeTrain,twitch);
     force = force_temp(1:length(t));
     
+    meanForce(i) = mean(force(2.5*Fs:3*Fs));
+    P2PForce(i) = max(force(2*Fs:3*Fs))-min(force(2*Fs:3*Fs));
+    
     figure(1)
+    plot(twitch)
+    hold on
+    figure(2)
     plot(t,force)
     hold on
-    
-    meanForce(i) = mean(force(2*Fs:4*Fs));
-    P2PForce(i) = max(force(2*Fs:4*Fs))-min(force(2*Fs:4*Fs));
 end
 
-
-figure(2)
-plot(FR/FR_half(testingUnit),meanForce./meanForce(end))
+figure(3)
+subplot(2,1,1)
+plot(FR/FR_half(testingUnit),T1)
 hold on
-plot(FR/FR_half(testingUnit),Af_vec)
+subplot(2,1,2)
+plot(FR/FR_half(testingUnit),T2)
+hold on
+
+figure(4)
+plot(FR/FR_half(testingUnit),meanForce)
 xlabel('Frequency (f_{0.5})','FontSize',14)
 ylabel('Force (AU)','FontSize',14)
-title('Corrected Frequency to Force')
-legend('New Algorithm','Original Algorithm')
 
-figure(3)
+figure(5)
 plot(FR/FR_half(testingUnit),P2PForce)
 xlabel('Frequency (f_{0.5})','FontSize',14)
-ylabel('Peak-to-peak Amplitude (AU)','FontSize',14)
-
-
+ylabel('Force (AU)','FontSize',14)
 
 %% function used
 function [twitch,T1,T2_temp] = twitch_function(f_env,Af,Lce,CT,RT,Fs)
-T1 = CT*Lce^2+(CT*1/2)*Af;
+T1 = CT*Lce^2+(CT*1/2)*f_env;
 T2_temp = (RT + (RT*1/2)*Af)/Lce;
 T2 = T2_temp/1.68;
 t_twitch = 0:1/Fs:1;
@@ -129,8 +136,8 @@ Af = 1 - exp(-(S*f_eff/(a_f*n_f))^n_f);
 end
 
 function FF = frequency2Force_slow_function(f_env,L,Y)
-a_f = 0.52;
-n_f0 = 1.97;
+a_f = 0.46;
+n_f0 = 2.17;
 n_f1 = 6.1;
 n_f = n_f0 + n_f1*(1/L-1);
 FF = 1 - exp(-(Y*f_env/(a_f*n_f))^n_f);
@@ -138,8 +145,8 @@ FF = FF/f_env;
 end
 
 function FF = frequency2Force_fast_function(f_env,L,S)
-a_f = 0.52;
-n_f0 = 1.97;
+a_f = 0.46;
+n_f0 = 2.17;
 n_f1 = 4;
 n_f = n_f0 + n_f1*(1/L-1);
 FF = 1 - exp(-(S*f_env/(a_f*n_f))^n_f);
